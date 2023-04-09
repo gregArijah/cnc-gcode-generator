46d391b8ae8bbf857f1eda92853a1cd7fc0e7d31
Greg Griffith <griffith_g@hotmail.com>
2023-04-09 04:50:10 -0400

    trying gh pages again

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'javatrol',
+  base: '/javatrol',
   plugins: [
     react(),
     tailwindcss(),
