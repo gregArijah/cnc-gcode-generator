739b6c9986e682a6414c0370f2d86579da7f5a5d
Greg Griffith <griffith_g@hotmail.com>
2023-04-09 21:56:53 -0400

    added coming soon page at new route

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,17 +1,19 @@
 //import bgImage from '../assets/images/background.jpg';
 
 function Main() {
-  return (
-    <div
-      className="h-screen bg-cover flex flex-col justify-center items-center"
-     // style={{ backgroundImage: `url(${bgImage})` }}
-    >
-      <h1 className="text-4xl text-white font-bold mb-8">Coming Soon!</h1>
-      <p className="text-xl text-white font-medium text-center max-w-md">
-        We're working hard to bring you an amazing application. Stay tuned for
-        updates!
-      </p>
-    </div>
+    return (
+        <div className="bg-jvt-outside bg-cover text-white min-h-screen max-w-full bg-center flex flex-col justify-center md:justify-end items-center md:pb-8">
+          <div className='border-solid border-4 rounded-2xl p-12 w-min md:w-max min-h-min' style={{backgroundColor: 'rgba(0, 0, 0, 0.65)', overflowY: 'auto', maxHeight: '500px' }}>
+            <div className='opacity-100 space-y-5'>   
+                <h1 className="text-4xl text-white font-bold mb-16 md:mb-0 text-center ">Coming Soon!</h1>
+                <p className="text-xl md:text-2xl text-white font-medium pt-20 md:pt-14 text-center max-w-md">
+                We're working hard to bring you an amazing application. Stay tuned for
+                updates!
+                </p>
+            </div>
+            </div>
+
+        </div>
   );
 }
 
