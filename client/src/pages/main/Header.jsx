1793dbf12b1b28783b1192c5eb1b3373b31e85d9
Greg Griffith <griffith_g@hotmail.com>
2023-04-16 18:17:19 -0400

    created separate module for header of main page

---
+++ client/src/pages/main/Header.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+
+import { dropIcon, newIcon, openIcon, dlIcon, trashIcon  } from './FontAwesome';
+import { chatIcon, settingsIcon, helpIcon, exitIcon } from './FontAwesome';
+
+export default function Header (){
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen);
+      };
+
+    return(    
+        <div>
+            <header className="h-16 flex justify-between items-center font-bold text-xl border-b mb-4 pb-4">
+                <div>Javatrol</div>
+                <div className="pr-4 md:pr-6">              
+                    <ul className='flex px-2 font-thin space-x-6'>
+                    <div className="relative">
+                        <button type="button" className="font-semibold px-4 rounded items-center hover:bg-gray-800 focus:outline-none" onClick={toggleDropdown} >
+                        My Projects {dropIcon}  
+                        </button>
+                        {isOpen && (
+                        <ul className="absolute z-10 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                            <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{newIcon} New</a></li>
+                            <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{openIcon} Open</a></li>
+                            <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{trashIcon} Delete</a></li>
+                            <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{dlIcon} Download</a></li>
+                        </ul>
+                        )}
+                        </div>
+                        <li>Chat {chatIcon}</li>
+                        <li>Settings {settingsIcon}</li>
+                        <li>Help {helpIcon}</li>
+                        <li>Exit {exitIcon}</li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+)}
--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect, useRef }  from 'react';
 import { Link } from 'react-router-dom';
 
-import { dropIcon, newIcon, openIcon, dlIcon, trashIcon  } from './FontAwesome';
-import { chatIcon, settingsIcon, helpIcon, exitIcon } from './FontAwesome';
+
+//import icons
+
 import { libraryIcon, codeIcon, dataIcon, robotIcon } from './FontAwesome';
 import { plusIcon } from './FontAwesome';
 
+//import header
+import Header from './Header';
+
 function Main() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleDropdown = () => {
-      setIsOpen(!isOpen);
-    };
+ 
 
   const [currentTab, setCurrentTab] = useState("G-code");
 
@@ -19,30 +20,9 @@ function Main() {
   return (
     <div className="min-h-screen bg-gray-900 text-white p-4">
       {/* Header */}
-      <header className="h-16 flex justify-between items-center font-bold text-xl border-b mb-4 pb-4">
-        <div>Javatrol</div>
-        <div className="pr-4 md:pr-6">              
-                <ul className='flex px-2 font-thin space-x-6'>
-                  <div className="relative">
-                    <button type="button" className="font-semibold px-4 rounded items-center hover:bg-gray-800 focus:outline-none" onClick={toggleDropdown} >
-                      My Projects {dropIcon}  
-                    </button>
-                    {isOpen && (
-                      <ul className="absolute z-10 right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-                        <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{newIcon} New</a></li>
-                        <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{openIcon} Open</a></li>
-                        <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{trashIcon} Delete</a></li>
-                        <li onClick={()=>setIsOpen(!isOpen)}><a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">{dlIcon} Download</a></li>
-                      </ul>
-                    )}
-                  </div>
-                  <li>Chat {chatIcon}</li>
-                  <li>Settings {settingsIcon}</li>
-                  <li>Help {helpIcon}</li>
-                  <li>Exit {exitIcon}</li>
-              </ul>
-          </div>
-      </header>
+      
+      {<Header />}  
+    
 
       {/* Body */}
       <div className="flex flex-grow">  
