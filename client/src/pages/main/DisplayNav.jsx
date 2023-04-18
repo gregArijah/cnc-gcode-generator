3b9c2bf1d6b69e1a7e34d265331118380484c8a9
Greg Griffith <griffith_g@hotmail.com>
2023-04-17 23:26:29 -0400

    highlight  metadata when intial operation is selected

--- client/src/pages/main/DisplayNav.jsx
+++ client/src/pages/main/DisplayNav.jsx
@@ -6,15 +6,16 @@ export default function Navlinks({ currentTab, setCurrentTab }){
     return(
         <nav className="flex-grow">
             <ul className="flex border-b">
+              <li className={`mr-1 py-2 px-4 cursor-pointer ${currentTab === 'Meta' ? 'bg-gray-700' : 'text-white hover:bg-gray-800'}`} onClick={() => setCurrentTab('Meta')}>
+                Metadata {dataIcon}
+              </li>
               <li className={`mr-1 py-2 px-4 cursor-pointer ${currentTab === 'G-code' ? 'bg-gray-700' : 'text-white hover:bg-gray-800'}`} onClick={() => setCurrentTab('G-code')}>
                 G-code {codeIcon}
               </li>
               <li className={`mr-1 py-2 px-4 cursor-pointer ${currentTab === 'Simulate' ? 'bg-gray-700' : 'text-white hover:bg-gray-800'}`} onClick={() => setCurrentTab('Simulate')}>
                 Simulate {robotIcon}
               </li>
-              <li className={`mr-1 py-2 px-4 cursor-pointer ${currentTab === 'Meta' ? 'bg-gray-700' : 'text-white hover:bg-gray-800'}`} onClick={() => setCurrentTab('Meta')}>
-                Metadata {dataIcon}
-              </li>
+              
               <li className={`mr-1 py-2 px-4 cursor-pointer ${currentTab === 'List' ? 'bg-gray-700' : 'text-white hover:bg-gray-800'}`} onClick={() => setCurrentTab('List')}>
                 Tool List {listIcon}
               </li>
