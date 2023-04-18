6b9625b1f8bf6b07c8001f6a88e390e3f233b1da
Greg Griffith <griffith_g@hotmail.com>
2023-04-17 22:12:35 -0400

    added an operations object to test operations

--- client/src/pages/main/DisplaySide.jsx
+++ client/src/pages/main/DisplaySide.jsx
@@ -3,6 +3,13 @@ import { plusIcon } from '../../icons/FontAwesome';
 export default function Sidebar({ currentOperation, setCurrentOperation }){
     const operations = ["centre drill","drill for 1/2-13 holes","chamfer","tap 1/2-13 holes",""];
 
+    const op1 = {
+        "name":"centre drill",
+        "desc":"centre drill for 1/2 holes",
+        "tool":"centre drill",
+        "gcode":"gcode goes here for centre drilling 1/2 holes"
+    }
+
     return (
         <div className="w-64 bg-gray-800 border rounded-md" style={{height:"80vh", overflowY: 'auto',}}>
             <div className="h-16 flex justify-center items-center font-bold text-xl border-b" >
@@ -10,9 +17,11 @@ export default function Sidebar({ currentOperation, setCurrentOperation }){
             </div>
             <ul className='pl-4'>
                 {operations.map((item,index)=>{
-                    return <li key={index} className= {`mr-1 py-0.5 cursor-pointer ${currentOperation === `op${index+1}` ? 'text-orange-500' : 'text-white hover:text-orange-700'}`} onClick={() => setCurrentOperation(`op${index+1}`)}>{index+1}. {item}</li>
+                     return <li key={index} className= {`mr-1 py-0.5 cursor-pointer ${currentOperation === `op${index+1}` ? 'text-orange-500' : 'text-white hover:text-orange-700'}`} onClick={() => setCurrentOperation(`op${index+1}`)}>{index+1}. {item}</li>
+                     {/* <p>{op1.name}</p> */}
                 })}
             </ul>  
+            
             <button className='border-t border-b min-w-full mt-2 p-1 pl-4 bg-gray-900 text-left'>{plusIcon} add operation</button>      
         </div>
     )
