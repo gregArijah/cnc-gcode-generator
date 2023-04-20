a44a4196b5c22f48e261eceaf5bbcfc715c63a68
Greg Griffith <griffith_g@hotmail.com>
2023-04-19 22:32:55 -0400

    testing out loading operations data from object array

--- client/src/pages/main/AppSidebar.jsx
+++ client/src/pages/main/AppSidebar.jsx
@@ -1,28 +1,28 @@
 import { plusIcon } from '../../icons/FontAwesome';
 
-//    const operations = [
-//       {
-//         name:"centre drill",
-//         desc:"centre drill for 1 inch holes",
-//         tool:"centre drill",
-//         gcode:"gcode goes here for centre drilling 1 inch holes"
-//         },
-//       {
-//         name:"pilot drill",
-//         desc:"pilot drill for 1 inch holes",
-//         tool:"5/8 drill",
-//         gcode:"gcode goes here for pilot drilling 1 inch holes with 5/8 drill"
-//         },
-//       {
-//         name:"drill",
-//         desc:"drill for 1 inch holes",
-//         tool:"1 inch drill",
-//         gcode:"gcode goes here for drilling 1 inch holes"
-//         }
-//     ]
+   const operations = [
+      {
+        name:"centre drill",
+        desc:"centre drill for 1 inch holes",
+        tool:"centre drill",
+        gcode:"gcode goes here for centre drilling 1 inch holes"
+        },
+      {
+        name:"pilot drill",
+        desc:"pilot drill for 1 inch holes",
+        tool:"5/8 drill",
+        gcode:"gcode goes here for pilot drilling 1 inch holes with 5/8 drill"
+        },
+      {
+        name:"drill",
+        desc:"drill for 1 inch holes",
+        tool:"1 inch drill",
+        gcode:"gcode goes here for drilling 1 inch holes"
+        }
+    ]
     
 export default function Sidebar({ currentOperation, setCurrentOperation }){
-    const operations = ["centre drill","drill for 1/2-13 holes","chamfer","tap 1/2-13 holes",""];
+   // const operations = ["centre drill","drill for 1/2-13 holes","chamfer","tap 1/2-13 holes",""];
  
 
     return (
@@ -32,8 +32,7 @@ export default function Sidebar({ currentOperation, setCurrentOperation }){
             </div>
             <ul className='pl-4'>
                 {operations.map((item,index)=>{
-                     return <li key={index} className= {`mr-1 py-0.5 cursor-pointer ${currentOperation === `op${index+1}` ? 'text-orange-500' : 'text-white hover:text-orange-700'}`} onClick={() => setCurrentOperation(`op${index+1}`)}>{index+1}. {item}</li>
-                     {/* <p>{op1.name}</p> */}
+                    return <li key={index} className= {`mr-1 py-0.5 cursor-pointer ${currentOperation === `op${index+1}` ? 'text-orange-500' : 'text-white hover:text-orange-700'}`} onClick={() => setCurrentOperation(`op${index+1}`)}>{index+1}. {item.name}</li>
                 })}
             </ul>  
             
