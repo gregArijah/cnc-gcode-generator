bce7e823159b524980365dd0812bd70ea7ca5252
Greg Griffith <griffith_g@hotmail.com>
2023-04-22 08:09:02 -0400

    all options listed on selection page

--- client/src/pages/main/SelectOpType.jsx
+++ client/src/pages/main/SelectOpType.jsx
@@ -7,18 +7,43 @@ export default function SelectOpType({ isOpen, onClose}){
                 <div className="text-2xl font-medium mb-4">Select an Operation</div>
                 <p className="mb-4 text-lg font-medium">Point</p>
                 <ul className="flex space-x-6">
-                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Drill</p></li>       
-                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Ream</p></li>       
-                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Tap</p></li>       
-                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Threadmill</p></li>       
-                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png"></img><p className="mb-4 text-md font-medium">Chamfer</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Drill</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Ream</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Tap</p></li>             
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Chamfer</p></li>
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Threadmill</p></li>        
                 </ul>
                 <hr />
                 <p className="mb-4 text-lg font-medium">Line</p>
+                <ul className="flex space-x-6">
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Centre</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Left</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Right</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Outside</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Inside</p></li>            
+                </ul>
                 <hr />
                 <p className="mb-4 text-lg font-medium">Face</p>
+                <ul className="flex space-x-6">
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Face</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Boss</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Slot</p></li> 
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Pocket</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Pocket-Boss</p></li>       
+                               
+                </ul>
                 <hr />
                 <p className="mb-4 text-lg font-medium">Turning</p>
+                <ul className="flex space-x-6">
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Bar</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Copy</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Corner</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Facing</p></li>                  
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Grooving</p></li>            
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Drilling</p></li>            
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Tapping</p></li>
+                    <li><img src="https://image-placeholder.com/images/actual-size/57x57.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Threading</p></li>                         
+                </ul>
 
                 <button onClick={onClose} className="rounded border-2 p-3 px-10 bg-gray-950 hover:text-orange-500  hover:border-orange-500">Cancel</button>
             </MyModal>    
