c48c17905802a1ff485d34e1c145c116fce96ed2
Greg Griffith <griffith_g@hotmail.com>
2023-04-21 22:44:47 -0400

    added first line of of sselection page

--- client/src/pages/main/Main.jsx
+++ client/src/pages/main/Main.jsx
@@ -14,7 +14,7 @@ function Main() {
 
   
   return (
-    <div className="min-h-screen bg-gray-900 text-white p-4">  
+    <div className="min-h-screen bg-gray-900 text-white p-4">
       {<Header />}  
       <div className="flex flex-grow">  
           {<Sidebar currentOperation={currentOperation} setCurrentOperation={setCurrentOperation} />}
--- client/src/pages/main/SelectOpType.jsx
+++ client/src/pages/main/SelectOpType.jsx
@@ -4,9 +4,23 @@ import MyModal from "../../components/modal"
 export default function SelectOpType({ isOpen, onClose}){
     return ( 
             <MyModal isOpen={isOpen} onClose={onClose}>
-                <div className="text-lg font-medium mb-4">Modal Title</div>
-                <p className="mb-4">Modal content goes here.</p>
-                <button onClick={onClose}>Close Modal</button>
+                <div className="text-2xl font-medium mb-4">Select an Operation</div>
+                <p className="mb-4 text-lg font-medium">Point</p>
+                <ul className="flex space-x-6">
+                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Drill</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Ream</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Tap</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png" alt="placeholder" /><p className="mb-4 text-md font-medium">Threadmill</p></li>       
+                    <li><img src="https://image-placeholder.com/images/actual-size/75x75.png"></img><p className="mb-4 text-md font-medium">Chamfer</p></li>       
+                </ul>
+                <hr />
+                <p className="mb-4 text-lg font-medium">Line</p>
+                <hr />
+                <p className="mb-4 text-lg font-medium">Face</p>
+                <hr />
+                <p className="mb-4 text-lg font-medium">Turning</p>
+
+                <button onClick={onClose} className="rounded border-2 p-3 px-10 bg-gray-950 hover:text-orange-500  hover:border-orange-500">Cancel</button>
             </MyModal>    
     )
 } 
