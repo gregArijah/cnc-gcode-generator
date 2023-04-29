0120fdf7b7c20dd2d7cfb863bbf156350b4a8e2d
Greg Griffith <griffith_g@hotmail.com>
2023-04-29 14:13:30 -0400

    developing drilling modal

--- client/src/pages/main/SelectDrill.jsx
+++ client/src/pages/main/SelectDrill.jsx
@@ -10,11 +10,8 @@ export default function SelectDrill({ isOpen, onClose, selectMainClose}){
 
     const [formData, setFormData] = useState({
         holeDiameter: "",
-        startZ: "",
-        finishZ: "",
+        holeDepth: "",
         chamferWidth: "",
-        xPos: "",
-        yPos: "",
       });
 
     const handleInputChange = (e) => {
@@ -29,11 +26,6 @@ export default function SelectDrill({ isOpen, onClose, selectMainClose}){
             holeDiameter: "",
             holeDepth: "",
             chamferWidth: "",
-
-            startZ: "",
-            finishZ: "",
-            xPos: "",
-            yPos: "",
         });
         onClose();
         selectMainClose();    
@@ -46,45 +38,110 @@ export default function SelectDrill({ isOpen, onClose, selectMainClose}){
 
 
     return (
-     
-        
             <MyModal isOpen={isOpen} onClose={onClose}>   
                 <div className="text-2xl font-medium mb-4">Drilling</div>
                 <form className="flex flex-col max-w-fit">   
-                <div className="flex justify-between">Include spot face<MyToggle enabled={enabled_spot} setEnabled={setEnabled_spot}/></div>
-                <div className="flex justify-between">Include centre drill<MyToggle enabled={enabled_centre} setEnabled={setEnabled_centre}/></div>
-                <div className="flex justify-between">Include drill<MyToggle enabled={enabled_drill} setEnabled={setEnabled_drill}/></div>
-                <div className="flex justify-between">Include chamfer drill<MyToggle enabled={enabled_chamfer} setEnabled={setEnabled_chamfer}/></div>
+                    <div className="flex flex-col max-w-fit">
+                        <label htmlFor="holeDiameter">Hole Diameter</label>
+                        <input
+                        type="text"
+                        id="holeDiameter"
+                        name="holeDiameter"
+                        value={formData.holeDiameter}
+                        onChange={handleInputChange}
+                        className="text-black"
+                        />
+                        <label htmlFor="holeDepth">Hole Depth</label>
+                        <input
+                        type="text"
+                        id="holeDepth"
+                        name="holeDepth"
+                        value={formData.holeDepth}
+                        onChange={handleInputChange}
+                        className="text-black"
+                        />  
+                        <label htmlFor="chamferWidth">Chamfer Width</label>
+                        <input
+                        type="text"
+                        id="chamferWidth"
+                        name="chamferWidth"
+                        value={formData.chamferWidth}
+                        onChange={handleInputChange}
+                        className="text-black"
+                        />
+                </div>
+                <br />
+                <div className="flex">
+                    <div className="space-y-1">
+                    <div className="flex justify-between">Include spot face<MyToggle enabled={enabled_spot} setEnabled={setEnabled_spot}/></div>
+                    <div className="flex justify-between">Include centre drill<MyToggle enabled={enabled_centre} setEnabled={setEnabled_centre}/></div>
+                    <div className="flex justify-between">Include drill<MyToggle enabled={enabled_drill} setEnabled={setEnabled_drill}/></div>
+                    <div className="flex justify-between">Include chamfer drill<MyToggle enabled={enabled_chamfer} setEnabled={setEnabled_chamfer}/></div>
+                    </div>
+                    <div>
+                    
+                    <div className="flex justify-between">
+                        <div className={`flex max-w-fit ${enabled_spot? '' : 'hidden'}`}>
+                            <label htmlFor="spotDiameter"> Tool Dia.</label>
+                            <input
+                                type="text"
+                                id="spotDiameter"
+                                name="spotDiameter"
+                                value={formData.drillDiameter}
+                                onChange={handleInputChange}
+                                className="text-black w-12"
+                            />
+                            <label htmlFor="spotDiameter"> Hole Dia.</label>
+                            <input
+                                type="text"
+                                id="spotDiameter"
+                                name="spotDiameter"
+                                value={formData.drillDiameter}
+                                onChange={handleInputChange}
+                                className="text-black w-12"
+                            />
+                            <label htmlFor="spotDiameter"> Angle </label>
+                            <input
+                                type="text"
+                                id="spotDiameter"
+                                name="spotDiameter"
+                                value={formData.drillDiameter}
+                                onChange={handleInputChange}
+                                className="text-black w-12"
+                            />
+                            <label htmlFor="spotDiameter"> FeedRate</label>
+                            <input
+                                type="text"
+                                id="spotDiameter"
+                                name="spotDiameter"
+                                value={formData.drillDiameter}
+                                onChange={handleInputChange}
+                                className="text-black w-12"
+                            />
+                            <label htmlFor="spotDiameter"> SpeedRate</label>
+                            <input
+                                type="text"
+                                id="spotDiameter"
+                                name="spotDiameter"
+                                value={formData.drillDiameter}
+                                onChange={handleInputChange}
+                                className="text-black w-12"
+                            />
+                            <label htmlFor="spotDiameter"> Coolant</label>
+                            <input
+                                type="text"
+                                id="spotDiameter"
+                                name="spotDiameter"
+                                value={formData.drillDiameter}
+                                onChange={handleInputChange}
+                                className="text-black w-12"
+                            />
+                        </div>
+                      </div>  
+                    </div>    
+                </div>
                 <br/>
-                <label htmlFor="holeDiameter">Hole Diameter</label>
-                <input
-                    type="text"
-                    id="holeDiameter"
-                    name="holeDiameter"
-                    value={formData.holeDiameter}
-                    onChange={handleInputChange}
-                    className="text-black"
-                />
-
-                <label htmlFor="holeDepth">Hole Depth</label>
-                <input
-                    type="text"
-                    id="holeDepth"
-                    name="holeDepth"
-                    value={formData.holeDepth}
-                    onChange={handleInputChange}
-                    className="text-black"
-                />  
                 
-                <label htmlFor="chamferWidth">Chamfer Width</label>
-                <input
-                    type="text"
-                    id="chamferWidth"
-                    name="chamferWidth"
-                    value={formData.chamferWidth}
-                    onChange={handleInputChange}
-                    className="text-black"
-                />
                 
                 <div className="space-x-2 mt-4 flex justify-between">
                 <button onClick={handleSubmit} className="rounded border-2 p-2 px-4 mt-2 bg-gray-950 hover:text-orange-500  hover:border-orange-500">Submit</button>
