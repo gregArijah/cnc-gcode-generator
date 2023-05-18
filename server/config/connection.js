4935b6142343be25075dbed277697c1994f1caf1
Greg Griffith <griffith_g@hotmail.com>
2023-05-17 22:39:44 -0400

    working on server files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,15 @@
+//import libraries
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/javatrol', {
+//define connection string
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/javatrol';
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to the database');
+}).catch((error) => {
+  console.error('Error connecting to the database:', error);
 });
 
 module.exports = mongoose.connection;
