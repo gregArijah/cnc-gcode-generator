64af438c115d416ce35e52e5cc5d728b7cc4f05b
Greg Griffith <griffith_g@hotmail.com>
2023-05-18 18:40:56 -0400

    added index.js to models folder

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./User');
+const Project = require('./Project');
+const Tool = require('./Tool');
+const Operation = require('./Operation');
+
+module.exports = { User, Project, Tool, Operation };
