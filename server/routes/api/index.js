0e30a791f6c92a451285ae5667e6883bd5a2c1d4
Greg Griffith <griffith_g@hotmail.com>
2023-05-18 18:45:57 -0400

    added index.js to routes/api folder

---
+++ server/routes/api/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const userRoutes = require('./userRoutes');
+const operationRoutes = require('./operationRoutes');
+const toolRoutes = require('./toolRoutes');
+const projectRoutes = require('./projectRoutes');
+
+router.use('/users', userRoutes);
+router.use('/operations', operationRoutes);
+router.use('/tools', toolRoutes);
+router.use('/projects', projectRoutes);
+
+
+module.exports = router;
