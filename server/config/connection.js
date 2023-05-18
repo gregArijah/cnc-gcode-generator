a46fc176a34d70e8b0bae59b378822332ec602c2
Greg Griffith <griffith_g@hotmail.com>
2023-05-18 16:03:47 -0400

    edit connection.js in config

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 //define connection string
 const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/javatrol';
+
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
