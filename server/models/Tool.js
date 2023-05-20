b56d390e4fa05348b640565091cbf8420f41600b
Greg Griffith <griffith_g@hotmail.com>
2023-05-20 14:12:51 -0400

    update tool model

--- server/models/Tool.js
+++ server/models/Tool.js
@@ -1,41 +1,40 @@
 const { Schema, model } = require('mongoose');
 const moment = require('moment');
 
-const ToolSchema = new Schema({
-    toolId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-    },
-    toolNumber: {
-        type: Number, 
-        required: true,
-    },
-    toolType: {
-        type: String,
-        required: true,
-        trim: true,
-        enum: ['End Mill', 'Ball Mill', 'Drill', 'Tap', 'Reamer', 'Boring Bar', 'Face Mill', 'Fly Cutter', 'Insert', 'Other']
-    },
-    toolDiameter: {
-        type: Number,
-    },
-    toolRadius: {
-        type: Number,
-    },
-    toolNotes: {
-        type: String,
-        trim: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
-    }
+const toolSchema = new Schema({
+  toolNumber: {
+    type: Number,
+    required: true
+  },
+  toolType: {
+    type: String,
+    required: true,
+    trim: true,
+    enum: ['End Mill', 'Ball Mill', 'Drill', 'Tap', 'Reamer', 'Boring Bar', 'Face Mill', 'Fly Cutter', 'Insert', 'Other']
+  },
+  toolDiameter: {
+    type: Number
+  },
+  toolRadius: {
+    type: Number
+  },
+  toolNotes: {
+    type: String,
+    trim: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+  }
 });
 
-//initialize model
-const Tool = model('Tool', ToolSchema);
+const Tool = model('Tool', toolSchema);
 
-//export model
 module.exports = Tool;
 
