488efb9a9c6838edcbdbb9e452bcacb44e6af78f
Greg Griffith <griffith_g@hotmail.com>
2023-05-20 14:18:33 -0400

    update project  model

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,40 +1,28 @@
-//define project schema for mongoose
-
-const { Schema, model } = require('mongoose'); 
+const { Schema, model } = require('mongoose');
 const moment = require('moment');
 
-const ProjectSchema = new Schema({
-    projectId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-    },
-    projectName: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    projectDescription: {
-        type: String,
-        required: true,
-        trim: true 
-    },
-    projectTools: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Tool'
-    }],
-    operations: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Operation'
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
-    }
+const projectSchema = new Schema({
+  projectName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  projectDescription: {
+    type: String,
+    trim: true
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+  }
 });
 
-//initialize model
-const Project = model('Project', ProjectSchema);
+const Project = model('Project', projectSchema);
 
-//export model
 module.exports = Project;
