de98d58e7aa1b5674e15314c1361f4ca69bc609c
Greg Griffith <griffith_g@hotmail.com>
2023-05-20 14:27:15 -0400

    update user  model

--- server/models/User.js
+++ server/models/User.js
@@ -1,46 +1,50 @@
-//create user schema for mongoose
-
-const { Schema, Types, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 const moment = require('moment');
 
-const UserSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-    },
-    username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    // email: {
-    //     type: String,
-    //     required: true,
-    //     unique: true,
-    //     match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
-    // },
-    projects: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Project'
-    }],
-    toolLibrary: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Tool'
-    }],
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
-    }
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+  }
 });
 
-//initialize model
-const User = model('User', UserSchema);
+// Hash the password before saving
+userSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
 
-//export model
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// Helper method to compare passwords
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const User = model('User', userSchema);
+
 module.exports = User;
