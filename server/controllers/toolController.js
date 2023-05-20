11d9cd0daaf2414478c00ee587942909e70d527a
Greg Griffith <griffith_g@hotmail.com>
2023-05-20 19:19:26 -0400

    using insominia to test and debug api endpoints - toolcontroller

--- server/controllers/toolController.js
+++ server/controllers/toolController.js
@@ -1,13 +1,9 @@
-const { Tool } = require('../models');
+const { Tool, User } = require('../models');
 
 const toolController = {
     // get all tools
     getAllTools(req, res) {
         Tool.find({})
-            // .populate({
-            //     path: 'projects',
-            //     select: '-__v'
-            // })
             .select('-__v')
             .sort({ _id: -1 })
             .then(dbToolData => res.json(dbToolData))
@@ -21,10 +17,6 @@ const toolController = {
     // get one tool by id
     getToolById({ params }, res) {
         Tool.findOne({ _id: params.id })
-            .populate({
-                path: 'projects',
-                select: '-__v'
-            })
             .select('-__v')
             .then(dbToolData => {
                 // If no tool is found, send 404
@@ -43,18 +35,33 @@ const toolController = {
     },
 
     // create tool
-    createTool({ body }, res) {
-        Tool.create(body)
-            .then(dbToolData => res.json(dbToolData))
-            .catch(err => res.json(err));
+    createTool(req, res) {
+        Tool.create(req.body)
+            .then( dbToolData => {
+                User.findOneAndUpdate(
+                    { _id: req.body.userId }, 
+                    { $push: { toolLibrary: dbToolData._id } },
+                    { runValidators: true, new: true }
+                )
+                .then(dbUserData => {
+                    if (!dbUserData) {
+                        res.status(404).json({ message: 'No user found with this id!' });
+                        return;
+                    }
+                    res.json(dbToolData);
+                })
+                .catch(err => res.json(err));
+            })
+           
     },
+     
 
-    // update tool by id
-    updateTool({ params, body }, res) {
+    // update tool by
+    updateTool( req, res) {
         Tool.findOneAndUpdate(
-            { _id: params.id },
-            body,
-            { new: true, runValidators: true }
+            { _id: req.params.id },
+            { $set: req.body },
+            { runValidators: true, new: true }
         )
         .then(dbToolData => {
             if (!dbToolData) {
