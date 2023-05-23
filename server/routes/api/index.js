3e83e378023b2038cb312dd16c210d87159ac32a
Greg Griffith <griffith_g@hotmail.com>
2023-05-22 21:12:16 -0400

    added htttp prooxy middleware to server

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -5,9 +5,20 @@ const operationRoutes = require('./operationRoutes');
 const toolRoutes = require('./toolRoutes');
 const projectRoutes = require('./projectRoutes');
 
-router.use('/users', userRoutes);
+const  { authUser }  = require('../../utils/auth');
+router.use(setUser);
+
+router.use('/users', authUser , userRoutes);
 router.use('/operations', operationRoutes);
 router.use('/tools', toolRoutes);
 router.use('/projects', projectRoutes);
+
+function setUser(req, res, next) {
+    const userId = req.body.userId;
+    if (userId) {
+        req.user = userId;
+    }
+    next();
+}
 
 module.exports = router;
