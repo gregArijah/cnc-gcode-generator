61cac3a5d7d852e9d3223d958e269953958b75c2
Greg Griffith <griffith_g@hotmail.com>
2023-05-25 15:21:40 -0400

    debugging deployment - delete catch all route

--- server/server.js
+++ server/server.js
@@ -24,10 +24,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
-// Catch-all route for non-API routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
 
 //Use API routes
 app.use(routes);
