e3aea74574534f18a3b7e1b4bcf102a7677c26e2
Greg Griffith <griffith_g@hotmail.com>
2023-05-28 01:19:16 -0400

    re added server and connection files

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  optimizeDeps: { 
+    include: ['react-router-dom'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  }
+})
