eab27c63b98bf852394f6750f65c60938e2d87bc
Greg Griffith <griffith_g@hotmail.com>
2023-05-29 00:10:43 -0400

    debugging - load client routes on refresh

--- server/server.js
+++ server/server.js
@@ -22,6 +22,11 @@ app.use(express.json());
 //Serve static assets in production 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  }
+  );
 }
 
 
